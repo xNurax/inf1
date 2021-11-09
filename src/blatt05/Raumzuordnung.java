bd450d058cf4d33cfda034f255fa7442fe6d2e0d
kunde <kunde@192.168.0.164>
2021-11-09 13:02:13 +0100

    Fixing excercise 5.5

--- src/blatt05/Raumzuordnung.java
+++ src/blatt05/Raumzuordnung.java
@@ -5,8 +5,7 @@ public class Raumzuordnung {
 	private Raum raum; // Referenz auf Raum
 	private Person nutzer; // Referenz auf Nutzer/in
 	private String grund;
-	
-	
+
 	// TODO Grund für Zuordnung (z.B. "Büroraum", "Labor") als Zeichenkette
 	// ergänzen.
 	// Darf nicht NULL sein und muss mindestens aus 3 Zeichen bestehen.
@@ -14,13 +13,14 @@ public class Raumzuordnung {
 	// mit Zuordnungsgrund hinzufügen.
 
 	/** Konstruktor */
-	public Raumzuordnung(Raum raum, Person nutzer) {
-		if (raum == null || nutzer == null) {
+	public Raumzuordnung(Raum raum, Person nutzer, String grund) {
+		if (raum == null || nutzer == null || grund == null) {
 			System.out.println("Error, Person or raum cant be undefined!");
 			return;
 		} else {
 			this.raum = raum;
 			this.nutzer = nutzer;
+			this.grund = grund;
 		}
 	}
 
@@ -28,6 +28,9 @@ public class Raumzuordnung {
 		this.getNutzer().print();
 		System.out.print(" nutzt den Raum: ");
 		this.getRaum().print();
+		if (grund.length() > 3) {
+			System.out.print(" als " + this.grund);
+		}
 	}
 
 	Raum getRaum() {
@@ -38,24 +41,26 @@ public class Raumzuordnung {
 		return this.nutzer;
 	}
 
+	String getGrund() {
+		return this.grund;
+	}
+
 	public static void main(String[] a) {
 		// Bröckl nutzt den Raum E 212a
 		Person ub = new Person("Ulrich", "Br�ckl");
 		Raum e212a = new Raum("e 212a", 2);
-		Raumzuordnung ub2e212a = new Raumzuordnung(e212a, ub);
+		Raumzuordnung ub2e212a = new Raumzuordnung(e212a, ub, "B�ro");
 		ub2e212a.print();
 
 		// Bröckl nutzt außerdem den Raum E 201
-		Raumzuordnung ub2e201 = new Raumzuordnung(new Raum("e 201", 77, true), // keine Zwischenvariable für e201
-																				// nötig...
-				ub);
+		Raumzuordnung ub2e201 = new Raumzuordnung(new Raum("e 201", 77, true), ub, "B�ro");
 		// Den Raum dieser Zuordnung ausgeben:
 		System.out.print("\n     Au�erdem: ");
 		ub2e201.getRaum().print();
 		System.out.println("\n");
 
 		// Vogelsang nutzt den Raum E 209
-		Raumzuordnung hv2e209 = new Raumzuordnung(new Raum("e 209", 3), new Person("Holger", "Vogelsang"));
+		Raumzuordnung hv2e209 = new Raumzuordnung(new Raum("e 209", 3), new Person("Holger", "Vogelsang"), "Labor");
 		hv2e209.print();
 
 	}
